debb991697cbb990bc650b504867d063dd9de84c
shkim2948 <shkim2948@gmail.com>
2022-04-08 19:13:19 +0900

    update1

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -59,4 +59,5 @@ public class BoardController {
         return "/board/boardDetail";
     }
 
+
 }
---
+++ src/main/java/com/example/demo/controller/BoardUpdateForm.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+public class BoardUpdateForm {
+
+    private String title;
+
+    private String writer;
+
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
